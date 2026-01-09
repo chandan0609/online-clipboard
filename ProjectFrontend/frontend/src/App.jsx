671dc0dfa1d631459285cd791920e5dd560c6f94
AmanRaj1813 <amanrajkharwat@gmail.com>
2026-01-09 11:33:46 +0530

    Final frontend chenages

--- ProjectFrontend/frontend/src/App.jsx
+++ ProjectFrontend/frontend/src/App.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import ClipboardPage from "./pages/ClipboardPage.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
-function App() {
+function App({ mode, setMode }) {
+  
+
   return (
     <>
+      <Header mode={mode} setMode={setMode} />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/clipboard/:id" element={<ClipboardPage />} />
       </Routes>
+      <Footer mode={mode} setMode={setMode} />
     </>
   );
 }
